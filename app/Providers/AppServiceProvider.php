75a92f7824a94ee3a6bc2f42a8d3dc0d55d294d8
ZonkDBR <crazyzonkkopong@gmail.com>
2025-06-09 20:38:40 +0700

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\LokasiKos;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,10 +25,14 @@ class AppServiceProvider extends ServiceProvider
         View::composer('template.main', function ($view) {
             $currentLocationId = session('current_location_id');
             $locations = LokasiKos::all();
-    
+
             $view->with([
                 'locations' => $locations,
             ]);
         });
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
